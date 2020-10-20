4e900162909482bb9de1c91d01e9883011114212
arupam <sahaarupam37@gmail.com>
2020-10-20 11:45:42 +0530

    .

--- app/src/main/java/com/example/adginternals/MainActivity.java
+++ app/src/main/java/com/example/adginternals/MainActivity.java
@@ -26,29 +26,32 @@ public class MainActivity extends AppCompatActivity {
                 case 0:
                     //Toast.makeText(this, "home", Toast.LENGTH_SHORT).show();
                     selectedFragment = new HomeFragment();
+                    setTitle("Home");
                     break;
                 case 1:
                     //Toast.makeText(this, "alerts", Toast.LENGTH_SHORT).show();
                     selectedFragment = new AlertsFragment();
+                    setTitle("Alerts");
                     break;
                 case 2:
                     //Toast.makeText(this, "mom", Toast.LENGTH_SHORT).show();
                     selectedFragment = new MomFragment();
+                    setTitle("M.O.M");
                     break;
                 case 3:
                     //Toast.makeText(this, "profile", Toast.LENGTH_SHORT).show();
                     selectedFragment = new ProfileFragment();
+                    setTitle("Profile");
                     break;
             }
 
             //Frag Transaction
-
             getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,selectedFragment).commit();
 
 
         return true;
         });
-
+        setTitle("Home");
         getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new HomeFragment()).commit();
 
     }
