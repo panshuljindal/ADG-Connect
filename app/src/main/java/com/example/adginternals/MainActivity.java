1c1840d25f1a7ee46cc8b5642adeaaeaa22b2c17
arupam <sahaarupam37@gmail.com>
2020-10-20 11:27:35 +0530

    add fragments for bottom navbar

--- app/src/main/java/com/example/adginternals/MainActivity.java
+++ app/src/main/java/com/example/adginternals/MainActivity.java
@@ -21,22 +21,35 @@ public class MainActivity extends AppCompatActivity {
         smoothBottomBar = (SmoothBottomBar) findViewById(R.id.bottomBar);
 
         smoothBottomBar.setOnItemSelectedListener(i -> {
+            Fragment selectedFragment = null;
             switch (i){
                 case 0:
                     Toast.makeText(this, "home", Toast.LENGTH_SHORT).show();
+                    selectedFragment = new HomeFragment();
                     break;
                 case 1:
                     Toast.makeText(this, "alerts", Toast.LENGTH_SHORT).show();
+                    selectedFragment = new AlertsFragment();
                     break;
                 case 2:
                     Toast.makeText(this, "mom", Toast.LENGTH_SHORT).show();
+                    selectedFragment = new MomFragment();
                     break;
                 case 3:
                     Toast.makeText(this, "profile", Toast.LENGTH_SHORT).show();
+                    selectedFragment = new ProfileFragment();
                     break;
             }
 
+            //Frag Transaction
+
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,selectedFragment).commit();
+
+
         return true;
         });
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new HomeFragment()).commit();
+
     }
 }
