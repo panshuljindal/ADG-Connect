1f4599a545ff44ee60cd7927064f1725fe9526e9
arupam <sahaarupam37@gmail.com>
2020-10-20 11:36:39 +0530

    switched to constraint layout for fragments

--- app/src/main/java/com/example/adginternals/MainActivity.java
+++ app/src/main/java/com/example/adginternals/MainActivity.java
@@ -24,19 +24,19 @@ public class MainActivity extends AppCompatActivity {
             Fragment selectedFragment = null;
             switch (i){
                 case 0:
-                    Toast.makeText(this, "home", Toast.LENGTH_SHORT).show();
+                    //Toast.makeText(this, "home", Toast.LENGTH_SHORT).show();
                     selectedFragment = new HomeFragment();
                     break;
                 case 1:
-                    Toast.makeText(this, "alerts", Toast.LENGTH_SHORT).show();
+                    //Toast.makeText(this, "alerts", Toast.LENGTH_SHORT).show();
                     selectedFragment = new AlertsFragment();
                     break;
                 case 2:
-                    Toast.makeText(this, "mom", Toast.LENGTH_SHORT).show();
+                    //Toast.makeText(this, "mom", Toast.LENGTH_SHORT).show();
                     selectedFragment = new MomFragment();
                     break;
                 case 3:
-                    Toast.makeText(this, "profile", Toast.LENGTH_SHORT).show();
+                    //Toast.makeText(this, "profile", Toast.LENGTH_SHORT).show();
                     selectedFragment = new ProfileFragment();
                     break;
             }
