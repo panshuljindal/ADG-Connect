bd315fcec3571bcfdce1dc1434db572ac2a8c9eb
panshuljindal <60618820+panshuljindal@users.noreply.github.com>
2021-03-27 16:21:12 +0530

    Home Page Completed

--- app/src/main/java/com/adgvit/internals/HomeFragment.java
+++ app/src/main/java/com/adgvit/internals/HomeFragment.java
@@ -5,9 +5,11 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -36,12 +38,14 @@ public class HomeFragment extends Fragment {
     RecyclerView recyclerView1,recyclerViewNotification;
     View view;
     ArrayList<card1item> list1;
-    ArrayList<card2item> list2,sortedArrayList;
-    ArrayList<Integer> timeStamps;
+    ArrayList<card2item> list2Core,list2Team,finalArrayF,sortedArrayF;
+    ArrayList<Integer> timeStampsCore,timeStampsTeam,sortedFTime,timeF;
     DatabaseReference myref,myref1,myref2;
+    int count;
     String team;
     List<String> teamlist;
     String uid;
+    ConstraintLayout ui1;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -54,11 +58,20 @@ public class HomeFragment extends Fragment {
         view = inflater.inflate(R.layout.fragment_home, container, false);
         recyclerView1 = (RecyclerView) view.findViewById(R.id.recyclerview1);
         recyclerViewNotification= (RecyclerView) view.findViewById(R.id.recyclerviewNotifications);
+        ui1 = view.findViewById(R.id.emptyHomeLayout);
 
         list1 = new ArrayList<>();
-        list2 = new ArrayList<>();
-        timeStamps = new ArrayList<>();
-        sortedArrayList = new ArrayList<>();
+
+        list2Core = new ArrayList<>();
+        timeStampsCore = new ArrayList<>();
+
+        list2Team = new ArrayList<>();
+        timeStampsTeam = new ArrayList<>();
+
+        finalArrayF = new ArrayList<>();
+        sortedArrayF = new ArrayList<>();
+        sortedFTime = new ArrayList<>();
+        timeF = new ArrayList<>();
 
         loadData();
 
@@ -73,14 +86,34 @@ public class HomeFragment extends Fragment {
         String team1 = team.replace("[", "");
         String team2 = team1.replace("]", "");
         teamlist= new ArrayList<>(Arrays.asList(team2.split(", ")));
-
+        addData();
         firebase();
 
         adapter1();
         adapter2();
+        checkData();
         return view;
     }
 
+    public void checkData(){
+        Log.i("Sorted",String.valueOf(sortedArrayF.size()));
+        if (sortedArrayF.size()==0){
+            ui1.setVisibility(View.VISIBLE);
+            recyclerViewNotification.setVisibility(View.INVISIBLE);
+        }else {
+            ui1.setVisibility(View.INVISIBLE);
+            recyclerViewNotification.setVisibility(View.VISIBLE);
+        }
+    }
+    public void addData(){
+        String title="";
+        list1.add(new card1item(R.drawable.androidback,R.drawable.ic_android,"ANDROID",title));
+        list1.add(new card1item(R.drawable.mlback,R.drawable.ic_ml,"Machine Language",title));
+        list1.add(new card1item(R.drawable.designback,R.drawable.ic_design,"Design",title));
+        list1.add(new card1item(R.drawable.iosback,R.drawable.ic_ios,"iOS",title));
+        list1.add(new card1item(R.drawable.webback,R.drawable.ic_web,"Web Dev",title));
+        adapter1();
+    }
     public void firebase(){
         myref.addValueEventListener(new ValueEventListener() {
             @Override
@@ -121,8 +154,8 @@ public class HomeFragment extends Fragment {
         myref1.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
-                sortedArrayList.clear();
-                list2.clear();
+                timeStampsCore.clear();
+                list2Core.clear();
                 for (DataSnapshot ds: snapshot.getChildren()){
                     String uids = ds.child("users").getValue().toString();
                     //Log.i("uids",uids);
@@ -135,14 +168,21 @@ public class HomeFragment extends Fragment {
                         if (current >= date) {
                             //Log.i("Date","Date Matched");
                         } else {
-                            timeStamps.add(ad.getTime());
-                            list2.add(new card2item(title, time));
+                            timeStampsCore.add(ad.getTime());
+                            list2Core.add(new card2item(title, time));
                         }
                     }
                 }
                 myref2.addValueEventListener(new ValueEventListener() {
                     @Override
                     public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        timeStampsTeam.clear();
+                        list2Team.clear();
+
+                        finalArrayF.clear();
+                        sortedArrayF.clear();
+                        sortedFTime.clear();
+                        timeF.clear();
                         for (DataSnapshot ds : snapshot.getChildren()){
                             String uids = ds.child("users").getValue().toString();
                             //Log.i("uids",uids);
@@ -155,25 +195,50 @@ public class HomeFragment extends Fragment {
                                 if (current >= date) {
                                     //Log.i("Date","Date Matched");
                                 } else {
-                                    timeStamps.add(ad.getTime());
-                                    list2.add(new card2item(title, time));
+                                    timeStampsTeam.add(ad.getTime());
+                                    list2Team.add(new card2item(title, time));
                                 }
                             }
                         }
-                        Collections.sort(timeStamps);
-                        for (int i=0;i<timeStamps.size();i++){
-                            sortedArrayList.add(list2.get(timeStamps.indexOf(timeStamps.get(i))));
-                            if (i==3){
-                                break;
+
+                        finalArrayF = list2Core;
+                        finalArrayF.addAll(list2Team);
+                        timeF = timeStampsCore;
+                        timeF.addAll(timeStampsTeam);
+                        sortedFTime.addAll(timeF);
+                        Collections.sort(sortedFTime);
+                        Log.i("Time",timeF.toString());
+                        Log.i("Sorted",sortedFTime.toString());
+                        count =0;
+                        int start=0;
+                        for (int i=0;i<timeF.size();i++){
+
+                            int index=timeF.indexOf(sortedFTime.get(i));
+
+                            try {
+                                sortedArrayF.add(finalArrayF.get(index));
+                                //Log.i("Count",String.valueOf(count));
+
+                                if (i==3){
+                                    break;
+                                }
                             }
+                            catch (IndexOutOfBoundsException e){
+
+                            }
+
+
                         }
                         savaData();
                         adapter2();
+                        checkData();
                     }
 
                     @Override
                     public void onCancelled(@NonNull DatabaseError error) {
+                        checkData();
 
+                        adapter2();
                     }
                 });
 
@@ -182,6 +247,9 @@ public class HomeFragment extends Fragment {
             @Override
             public void onCancelled(@NonNull DatabaseError error) {
 
+                //savaData();
+                checkData();
+                adapter2();
             }
         });
     }
@@ -200,7 +268,8 @@ public class HomeFragment extends Fragment {
         recyclerView1.setLayoutManager(manager);
     }
     public void adapter2(){
-        card2adapter adapter1= new card2adapter(getContext(),sortedArrayList);
+        checkData();
+        card2adapter adapter1= new card2adapter(getContext(),sortedArrayF);
         LinearLayoutManager manager1= new LinearLayoutManager(getContext());
         manager1.setOrientation(RecyclerView.VERTICAL);
         recyclerViewNotification.setAdapter(adapter1);
@@ -211,33 +280,26 @@ public class HomeFragment extends Fragment {
         SharedPreferences.Editor editor = preferences.edit();
         Gson gson = new Gson();
         String json = gson.toJson(list1);
+        String json1 = gson.toJson(sortedArrayF);
         editor.putString("scrollbar",json);
-        String json1 = gson.toJson(list2);
         editor.putString("notifications",json1);
-        /*String json2 = gson.toJson(timeStamps);
-        editor.putString("timeStamps",json2);*/
         editor.apply();
+
     }
     public void loadData(){
         SharedPreferences preferences = view.getContext().getSharedPreferences("com.adgvit.com.home",Context.MODE_PRIVATE);
         Gson gson = new Gson();
         String json = preferences.getString("scrollbar","");
         String json1 = preferences.getString("notifications","");
-        String json2 = preferences.getString("timeStamps","");
         Type type = new TypeToken<ArrayList<card1item>>() {}.getType();
         Type type1 = new TypeToken<ArrayList<card2item>>() {}.getType();
-        Type type2 = new TypeToken<ArrayList<Integer>>() {}.getType();
         list1 =gson.fromJson(json,type);
         if(list1==null){
             list1 =new ArrayList<>();
         }
-        list2 = gson.fromJson(json1,type1);
-        if(list2==null){
-            list2 = new ArrayList<>();
+        sortedArrayF = gson.fromJson(json1,type1);
+        if (sortedArrayF==null){
+            sortedArrayF = new ArrayList<>();
         }
-        /*timeStamps = gson.fromJson(json2,type2);
-        if (timeStamps==null){
-            timeStamps = new ArrayList<>();
-        }*/
     }
 }
